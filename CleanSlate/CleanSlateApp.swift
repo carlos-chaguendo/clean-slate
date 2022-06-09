eaf34b2a3e394bc4c9991fef0d4a31d41c07cea6
carlos andres chaguendo <c.chaguendo@mayorgafirm.com>
2022-06-09 18:56:54 -0500

    Init
    Branch: #master
    master

---
+++ CleanSlate/CleanSlateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanSlateApp.swift
+//  CleanSlate
+//
+//  Created by Carlos Andres Chaguendo Sanchez on 9/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CleanSlateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
